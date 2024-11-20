44c7264d2fd545813e487bd69affa695cc4af29a
Ruth <165259669+eelut@users.noreply.github.com>
2024-11-20 22:00:21 +0300

    Time: 0 ms (100%), Space: 48.9 MB (55.75%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.js
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.js
@@ -3,9 +3,9 @@
  * @return {number}
  */
 var smallestEvenMultiple = function(n) {
-    if(n%2==0){
-        return n;
-    }else{
-        return n*2;
-    }
+   if(n%2===0){
+    return n;
+   }else{
+    return(n*2);
+   }
 };
