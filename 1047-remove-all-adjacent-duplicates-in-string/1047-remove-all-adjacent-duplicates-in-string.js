0650e7533021e7785155c3f767fdac28c1722ff5
Ruth <165259669+eelut@users.noreply.github.com>
2025-02-12 21:46:20 +0300

    Time: 11 ms (76.29%), Space: 58.4 MB (26.65%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.js
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.js
@@ -3,11 +3,13 @@
  * @return {string}
  */
 var removeDuplicates = function(s) {
-    for(var i=1; i<s.length; i++){
-        if(s[i] === s[i-1]){
-            s = s.slice(0, i-1) + s.slice(i+1);
-            i-=2;
-        }
+  let stack=[];
+  for(let char of s){
+    if(stack.length && stack[stack.length-1]===char){
+        stack.pop();
+    }else{
+        stack.push(char);
     }
-    return s;
+  }
+  return stack.join('');
 };
